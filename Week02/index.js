1f487ed52644505c16e365c5b863e659305286f4
tasjarutas <tasjarutas@gmail.com>
2021-02-26 17:11:07 +0000

    Update code using express and nodejs

--- Week02/index.js
+++ Week02/index.js
@@ -7,6 +7,7 @@ const port = 20000;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "images")));
 
 app.get("/", (req, res) => {
     res.render("index");
